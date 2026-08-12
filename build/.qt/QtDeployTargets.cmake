a93ed10fafc451444266fa01dcf3f1edc4ec036f
xiaoqinnb666 <xiaoqinnb666@users.noreply.github.com>
2026-08-12 11:54:13 +0800

    clean: remove build artifacts from repo

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_XiaoQinTools_FILE C:/XiaoQinTools/build/XiaoQinTools.exe)
-set(__QT_DEPLOY_TARGET_XiaoQinTools_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_XiaoQinTools_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Svg.dll;C:/msys64/mingw64/bin/Qt6QuickControls2.dll;C:/msys64/mingw64/bin/Qt6Concurrent.dll;C:/msys64/mingw64/bin/Qt6QuickTemplates2.dll;C:/msys64/mingw64/bin/Qt6Quick.dll;C:/msys64/mingw64/bin/Qt6OpenGL.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6QmlMeta.dll;C:/msys64/mingw64/bin/Qt6QmlWorkerScript.dll;C:/msys64/mingw64/bin/Qt6QmlModels.dll;C:/msys64/mingw64/bin/Qt6Qml.dll;C:/msys64/mingw64/bin/Qt6Network.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
